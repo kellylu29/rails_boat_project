c092f9b34bf3488dd39a067f966e610fc8bd7f3f
Kelly <kellylovelu@gmail.com>
2017-08-24 14:56:07 -0400

    add join table for users and boats. add views for boats pages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,25 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
